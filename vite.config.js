d0e01bc887bda2096df450759b424cfa2e26c5aa
Jocelyn-vnt <jocelyn.vinatie@etu.unilim.fr>
2024-11-19 16:32:23 +0100

    test-public

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/TV-MAZE/", 
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./public"),
     },
   },
 });
